cc77938bc6de156e6c9f4db5da3eb311f7908be1
RicardoTurco <ricturco@gmail.com>
2021-12-17 03:49:27 -0300

    add: main file (application)

---
+++ main.py
@@ -0,0 +1,22 @@
+import uvicorn
+from typing import Optional
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, p: bool, q: Optional[str] = None):
+    return {"item_id": item_id, "p": p, "q": q}
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
+
