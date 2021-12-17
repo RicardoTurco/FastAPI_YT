f9e66193eec20d9cd393b2db127e31bfb5f947a8
RicardoTurco <ricturco@gmail.com>
2021-12-17 03:53:52 -0300

    Aula 2

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import uvicorn
 from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    id: int
+    descricao: str
+    valor: float
 
 
 app = FastAPI()
@@ -16,7 +23,15 @@ def read_item(item_id: int, p: bool, q: Optional[str] = None):
     return {"item_id": item_id, "p": p, "q": q}
 
 
+@app.post("/item")
+def add_item(novo_item: Item):
+    return novo_item
+
+
+@app.post("/itens-bulk")
+def add_item_bulk(novo_item: Item, outro_item: Item):
+    return [novo_item, outro_item]
+
 
 if __name__ == "__main__":
     uvicorn.run(app)
-
