6ebf62797be592caeb51b499ef7d1005d95a20e5
RicardoTurco <ricturco@gmail.com>
2021-12-18 13:41:42 -0300

    fix: Adjusts in main file

--- main.py
+++ main.py
@@ -13,21 +13,31 @@ class Item(BaseModel):
 app = FastAPI()
 
 
+# Message of Wellcome
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello, I'm a simple example API with FastAPI"}
 
 
 # get standard HEADERS
+# In example, is recupered key "user-agent" ... but, is possible to retrieve any one
 @app.get("/headers")
-def read_root(user_agent: Optional[str] = Header(None)):
+def read_headers(user_agent: Optional[str] = Header(None)):
     return {"user_agent": user_agent}
 
 
 # get HEADERS entered by user
+
+# OBS:
+# Use Postman an example, the "key" must be the same
+# in function, with "_"
+
+# Ex:
+# "my-header-customized (Postman)
+# "my_headercustomized (function)
 @app.get("/headers-customized")
-def read_root(my_header: Optional[str] = Header(None)):
-    return {"my-header": my_header}
+def read_headers_customized(my_header_customized: Optional[str] = Header(None)):
+    return {"my_header": my_header_customized}
 
 
 # set COOKIE on browser
@@ -43,16 +53,24 @@ def get_cookie(meucookie: Optional[str] = Cookie(None)):
     return {"cookie": meucookie}
 
 
+# Get parameters in URL (and "query string")
+# remember: "q" is OPTIONAL ...
+
+# Ex:
+# localhost:8000/items/2/?p=true&q="msg"
 @app.get("/items/{item_id}")
 def read_item(item_id: int, p: bool, q: Optional[str] = None):
     return {"item_id": item_id, "p": p, "q": q}
 
 
+# Get a new instance of "Item"
+# OBS: The payload of body must be equal of "Item" model
 @app.post("/item")
 def add_item(novo_item: Item):
     return novo_item
 
 
+# Get a 2 instances of "Item"
 @app.post("/itens-bulk")
 def add_item_bulk(novo_item: Item, outro_item: Item):
     return [novo_item, outro_item]
